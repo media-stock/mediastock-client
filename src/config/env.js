23c2bcf1fbe205b1ff6c3475607019c7268574a8
alpoxdev <alpoxdev@gmail.com>
2020-12-13 22:20:30 +0900

    fix: endpoint

--- src/config/env.js
+++ src/config/env.js
@@ -3,5 +3,5 @@ export const isDev = NODE_ENV === 'development';
 
 export const API_URL = isDev
     ? 'http://localhost:8080'
-    : 'https://oy7hwv9o88.execute-api.ap-northeast-2.amazonaws.com/dev';
-export const SOCKET_URL = '';
+    : 'https://8r6rvn3xd8.execute-api.ap-northeast-2.amazonaws.com/dev';
+export const SOCKET_URL = 'wss://h40ubpc3jc.execute-api.ap-northeast-2.amazonaws.com/dev';
