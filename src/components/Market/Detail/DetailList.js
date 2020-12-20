03ce75a0a1fd8c577f4497427e4ff4169142d076
Kiung Lee <gi4389@konkuk.ac.kr>
2020-12-21 06:07:06 +0900

    add fuck해 우사인 우사인볼트가 - #1

--- src/components/Market/Detail/DetailList.js
+++ src/components/Market/Detail/DetailList.js
@@ -1,6 +1,94 @@
 import React from 'react';
+import styled, { css } from 'styled-components';
+import MarketDetailListItem from './DetailListItem';
 
 export default function MarketDetailList({ active }) {
     if (active === 'main') return null;
-    return <>Detail List</>;
+
+    return (
+        <ListWrapper>
+            <ListHeaderWrapper>
+                <ListHeaderItem changecolor left>
+                    채널명
+                    <br />
+                    카테고리
+                </ListHeaderItem>
+                <ListHeaderItem>현재가</ListHeaderItem>
+                <ListHeaderItem width="20%" changecolor>
+                    평가 손익
+                    <br />
+                    수익률
+                </ListHeaderItem>
+                <ListHeaderItem width="30%" right>
+                    거래량
+                </ListHeaderItem>
+            </ListHeaderWrapper>
+            <MarketDetailListItem />
+            <MarketDetailListItem />
+            <MarketDetailListItem />
+            <MarketDetailListItem />
+            <MarketDetailListItem />
+            <MarketDetailListItem />
+            <MarketDetailListItem />
+            <MarketDetailListItem />
+            <MarketDetailListItem />
+            <MarketDetailListItem />
+            <MarketDetailListItem />
+            <MarketDetailListItem />
+            <MarketDetailListItem />
+            <MarketDetailListItem />
+            <MarketDetailListItem />
+            <MarketDetailListItem />
+        </ListWrapper>
+    );
 }
+
+const ListWrapper = styled.div`
+    width: 100%;
+`;
+
+const ListHeaderWrapper = styled.div`
+    width: 100%;
+    height: 50px;
+    margin-bottom: 5px;
+
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+`;
+
+const ListHeaderItem = styled.div`
+    width: 25%;
+    height: 100%;
+
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+
+    font-size: 14px;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.13;
+
+    background-color: #f1f1f1;
+    color: #2e2e2e;
+
+    ${(props) =>
+        props.changecolor &&
+        css`
+            background-color: #2e2e2e;
+            color: #ffffff;
+        `}
+    ${(props) => {
+        const width = props?.width;
+        if (width) return `width : ${width};`;
+    }}
+    ${(props) => {
+        const left = props?.left;
+        const right = props?.right;
+        if (left) return `border-top-left-radius : 14px;`;
+        if (right) return `border-top-right-radius : 14px;`;
+    }}
+`;
--- src/components/Market/Detail/DetailListItem.js
+++ src/components/Market/Detail/DetailListItem.js
@@ -1,5 +1,93 @@
 import React from 'react';
+import styled from 'styled-components';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-regular-svg-icons';
 
 export default function MarketDetailListItem() {
-    return <></>;
+    return (
+        <ItemWrapper>
+            <Item>
+                <ProfileImg src="/sample-profile.png" />
+                <ProfileName>
+                    감스트 <br /> 인물
+                </ProfileName>
+            </Item>
+            <Item>55,000</Item>
+            <Item width="20%" color>
+                ▲5,000 <br /> +10%
+            </Item>
+            <Item width="30%" right>
+                10,000,000
+                <StarIcon icon={faStar} />
+            </Item>
+        </ItemWrapper>
+    );
 }
+
+const ItemWrapper = styled.div`
+    width: 100%;
+    height: 60px;
+    margin-bottom: 5px;
+
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+`;
+
+const Item = styled.div`
+    width: 25%;
+    height: 100%;
+
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+
+    font-size: 12px;
+    font-weight: bold;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.13;
+
+    background-color: #f8f8f8;
+    color: #000000;
+
+    ${(props) => {
+        const color = props?.color;
+        if (color) return ` color : #e10f0e;`;
+    }}
+
+    ${(props) => {
+        const width = props?.width;
+        if (width) return `width : ${width};`;
+    }}
+
+    ${(props) => {
+        const right = props?.right;
+        if (right) return `justify-content : flex-end`;
+    }}
+`;
+
+const ProfileImg = styled.img`
+    width: 50%;
+`;
+
+const ProfileName = styled.div`
+    width: 50%;
+
+    text-align: center;
+    font-weight: 600;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: 1.38;
+
+    color: #000000;
+`;
+
+const StarIcon = styled(FontAwesomeIcon)`
+    color: #ffd816;
+    margin-left: 5px;
+    margin-right: 5px;
+`;
