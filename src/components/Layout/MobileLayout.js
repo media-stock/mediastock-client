b3143a4656d33b305e2ab0f4f9dd3aaf8456cd08
alpoxdev <alpoxdev@gmail.com>
2021-01-20 02:24:58 +0900

    add: tabview ui design

--- src/components/Layout/MobileLayout.js
+++ src/components/Layout/MobileLayout.js
@@ -1,7 +1,12 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import styled from 'styled-components';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHome, faPlay } from '@fortawesome/free-solid-svg-icons';
+import { faThumbsUp, faCommentDots } from '@fortawesome/free-regular-svg-icons';
+
+import { useSelector } from 'react-redux';
 
 // container
 import LoginContainer from 'container/auth/login';
@@ -38,6 +43,8 @@ function MobileHeader() {
 
 function MobileFooter() {
     const router = useRouter();
+    const user = useSelector((state) => state.user.toJS().logined?.user);
+    const [myModal, setMyModal] = useState(false);
 
     const onClickItem = useCallback(
         (pathname, query = null) => {
@@ -52,18 +59,45 @@ function MobileFooter() {
         [router.query],
     );
 
+    const onClickMain = useCallback(() => {
+        if (!user) onClickItem(null, { auth: 'login' });
+        else setMyModal(!myModal);
+    }, [user, myModal]);
+
     return (
-        <MobileFooterWrapper>
-            <MobileFooterItem onClick={() => onClickItem('/')}>경매</MobileFooterItem>
-            <MobileFooterItem onClick={() => onClickItem('/market')}>미톡마켓</MobileFooterItem>
-            <MobileFooterItem onClick={() => onClickItem(null, { auth: 'login' })}>
-                Home
-            </MobileFooterItem>
-            <MobileFooterItem onClick={() => onClickItem('/article')}>
-                Media's talk
-            </MobileFooterItem>
-            <MobileFooterItem onClick={() => onClickItem('/more')}>더보기</MobileFooterItem>
-        </MobileFooterWrapper>
+        <>
+            <MobileFooterWrapper>
+                <MobileFooterItem onClick={() => onClickItem('/')}>
+                    <FontAwesomeIcon icon={faHome} size="lg" color="#FFBCBC" />
+                    <MobileFooterItemText>홈</MobileFooterItemText>
+                </MobileFooterItem>
+                <MobileFooterItem onClick={() => onClickItem('/market')}>
+                    <FontAwesomeIcon icon={faPlay} size="lg" color="#FFBCBC" />
+                    <MobileFooterItemText>미톡마켓</MobileFooterItemText>
+                </MobileFooterItem>
+                <MobileFooterItem onClick={onClickMain}>
+                    <MobileFooterMainIcon src="/tab-main-icon.png" />
+                </MobileFooterItem>
+                <MobileFooterItem onClick={() => onClickItem('/article')}>
+                    <FontAwesomeIcon icon={faThumbsUp} size="lg" color="#FFBCBC" />
+                    <MobileFooterItemText>Media's talk</MobileFooterItemText>
+                </MobileFooterItem>
+                <MobileFooterItem onClick={() => onClickItem('/more')}>
+                    <FontAwesomeIcon icon={faCommentDots} size="lg" color="#FFBCBC" />
+                    <MobileFooterItemText>더보기</MobileFooterItemText>
+                </MobileFooterItem>
+            </MobileFooterWrapper>
+
+            {myModal && (
+                <Hexagon>
+                    <HexagonImage src="/hexagon.png" />
+
+                    <HexagonText>관심채널</HexagonText>
+                    <HexagonText>나의 채널</HexagonText>
+                    <HexagonText>나의 경매</HexagonText>
+                </Hexagon>
+            )}
+        </>
     );
 }
 
@@ -112,6 +146,7 @@ const MobileFooterWrapper = styled.div`
 
     display: flex;
     align-items: center;
+    z-index: 2;
 
     background-color: white;
 
@@ -126,5 +161,66 @@ const MobileFooterItem = styled.div`
     height: 50px;
 
     display: flex;
+    flex-direction: column;
+    align-items: center;
     justify-content: center;
+`;
+
+const MobileFooterMainIcon = styled.img`
+    width: 80px;
+    margin-top: -35px;
+
+    object-fit: contain;
+`;
+
+const MobileFooterItemText = styled.div`
+    margin-top: 5px;
+
+    color: #ffbcbc;
+    font-size: 12.5px;
+    font-weight: bold;
+`;
+
+const Hexagon = styled.div`
+    width: 100%;
+    height: 179px;
+
+    position: fixed;
+    bottom: -33px;
+
+    display: flex;
+    justify-content: center;
+
+    z-index: 1;
+`;
+
+const HexagonImage = styled.img`
+    width: 204px;
+    height: 179px;
+    position: absolute;
+    top: 0;
+`;
+
+const HexagonText = styled.p`
+    font-size: 14px;
+    font-weight: bold;
+    color: #ffa8a8;
+    margin: 0;
+
+    position: absolute;
+    z-index: 2;
+
+    &:nth-child(2) {
+        margin-top: 40px;
+        margin-left: -120px;
+    }
+
+    &:nth-child(3) {
+        margin-top: 10px;
+    }
+
+    &:nth-child(4) {
+        margin-top: 40px;
+        margin-right: -120px;
+    }
 `;
